5a0858479540281a29e02a49ebece1fe3c062e48
jin1963 <kaojino89@gmail.com>
2025-07-14 00:53:35 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ConnectButton from "./components/ConnectButton";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Kaojino Wallet</h1>
+      <ConnectButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ config.js
@@ -0,0 +1,1 @@
+export const G3X24_ADDRESS = "0x65e47d9bd03c73021858ab2e1acb2cab38d9b039";
